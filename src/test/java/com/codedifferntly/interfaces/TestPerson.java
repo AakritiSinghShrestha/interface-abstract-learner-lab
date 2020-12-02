6df0a5c791f4947610ff80821ea4fe423636c733
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2020-12-01 19:14:47 -0500

    Done the test cases

--- src/test/java/com/codedifferntly/interfaces/TestPerson.java
+++ src/test/java/com/codedifferntly/interfaces/TestPerson.java
@@ -1,5 +1,48 @@
 package com.codedifferntly.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
+//Create a testConstructor method which ensures that a Person object's id field is being set upon construction.
+//Create a testSetName method which ensures that a Person object's name variable is being set by invoking the .setName method.
+
 public class TestPerson {
+    @Test
+    public void constructorIdTest(){
+        //Given
+        Person person = new Person(234l,"Mike");
+
+        //When
+long expected = 234l;
+long actual = person.getId();
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+
+
+    @Test
+    public void constructorNameTest(){
+        //Given
+        Person person = new Person(234l,"Mike");
+
+        //When
+        String expected = "Mike";
+        String actual = person.getName();
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void setNameTest(){
+        //Given
+        Person person = new Person(234l,"Mike");
+        person.setName("Sam");
+        //When
+        String expected = "Sam";
+        String actual = person.getName();
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
 
 }
